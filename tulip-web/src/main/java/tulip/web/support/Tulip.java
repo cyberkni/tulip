3409498c08ea8196b2d0d02fa8ccba5f9703030c
liufeiit <liufei_it@126.com>
2014-10-16 18:34:49 +0800

    ss

--- tulip-web/src/main/java/tulip/web/support/Tulip.java
+++ tulip-web/src/main/java/tulip/web/support/Tulip.java
@@ -1,5 +1,10 @@
 package tulip.web.support;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 
  * @author 刘飞 E-mail:liufei_it@126.com
@@ -8,17 +13,47 @@ package tulip.web.support;
  */
 public class Tulip {
 	
+	public static final String DEFAULT_TEMPLATES_BASE_URI = "WEB-INF/templates";
+	public static final String DEFAULT_TEMPLATES_SUFFIX = ".vm";
+	
+	public static final String DEFAULT_TEMPLATES_SCREEN_URI = "screen";
+	public static final String DEFAULT_TEMPLATES_LAYOUT_URI = "layout";
+	public static final String DEFAULT_TEMPLATES_DEFAULT_LAYOUT = "default";
+	public static final String DEFAULT_TEMPLATES_SCREEN_KEY = "screen_placeholder";
+	
 	protected static final String VELOCITY_ENGINE_TAG = "velocity-engine";
 	protected static final String VIEW_CONTROLLER_TAG = "view-controller";
 	protected static final String EVENT_CARTRIDGE_TAG = "event-cartridge";
 	protected static final String VIEW_RESOLVER_TAG = "view-resolver";
 	protected static final String COMMONS_TAG = "commons";
+
+	protected static final String CONTROLLER_URI_ATTR = "controller-uri";
+	protected static final String CONTROLLER_NAME_ATTR = "name";
+	protected static final String EVENT_HANDLER_TAG = "handler";
+	protected static final String TEMPLATES_SCREEN_KEY_TAG = "screen-key";
+	protected static final String TEMPLATES_DEFAULT_LAYOUT_TAG = "default-layout";
+	protected static final String TEMPLATES_LAYOUT_URI_TAG = "layout-uri";
+	protected static final String TEMPLATES_SCREEN_URI_TAG = "screen-uri";
+	protected static final String TEMPLATES_SUFFIX_TAG = "suffix";
+	protected static final String TEMPLATES_URI_TAG = "templates-uri";
 	
 	public Tulip() {
 		super();
 	}
 	
-	protected boolean auto_config;
-	protected String templates_uri;
-	protected String suffix;
+	protected String templates_uri = DEFAULT_TEMPLATES_BASE_URI;
+	protected String suffix = DEFAULT_TEMPLATES_SUFFIX;
+	
+	protected String screen_uri = DEFAULT_TEMPLATES_SCREEN_URI;
+	protected String layout_uri = DEFAULT_TEMPLATES_LAYOUT_URI;
+	protected String default_layout = DEFAULT_TEMPLATES_DEFAULT_LAYOUT;
+	protected String screen_key = DEFAULT_TEMPLATES_SCREEN_KEY;
+	
+	protected List<Class<?>> handlers = new ArrayList<Class<?>>();
+	
+	protected Map<String, String> controllers = new HashMap<String, String>();
+	
+	protected String config_location;
+	protected Class<?> velocity_engine_configurer;
+	
 }
--- tulip-web/src/main/java/tulip/web/support/TulipDefinitionParser.java
+++ tulip-web/src/main/java/tulip/web/support/TulipDefinitionParser.java
@@ -1,9 +1,5 @@
 package tulip.web.support;
 
-import java.util.HashMap;
-
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.MapConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -12,6 +8,7 @@ import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.ClassUtils;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
@@ -24,7 +21,7 @@ import tulip.util.StringUtil;
  * @since 2014年8月28日 下午4:20:30
  */
 public class TulipDefinitionParser extends Tulip implements BeanDefinitionParser {
-
+	
 	private final Log log = LogFactory.getLog(getClass());
 
 	public static void main0(String[] args) {
@@ -34,97 +31,81 @@ public class TulipDefinitionParser extends Tulip implements BeanDefinitionParser
 	@Override
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
 		parseConfiguration(element);
-		
-		
-		
+
 		DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) parserContext.getRegistry();
 
 		return null;
 	}
-	
+
 	private void parseConfiguration(Element element) {
-		Configuration configuration = new MapConfiguration(new HashMap<String, Object>());
-		auto_config = ConvertUtils.convert(element.getAttribute("auto-config"), boolean.class, true);
 		NodeList commonsList = element.getElementsByTagName(COMMONS_TAG);
 		int len = commonsList.getLength();
-		if(len > 0) {
+		if (len > 0) {
 			Element commons = (Element) commonsList.item(0);
-			templates_uri = tulipItem(commons, "templates-uri", "WEB-INF/templates");
-			suffix = tulipItem(commons, "suffix", ".vm");
+			templates_uri = parseItem(commons, TEMPLATES_URI_TAG, DEFAULT_TEMPLATES_BASE_URI);
+			suffix = parseItem(commons, TEMPLATES_SUFFIX_TAG, DEFAULT_TEMPLATES_SUFFIX);
 		}
 		NodeList viewResolverList = element.getElementsByTagName(VIEW_RESOLVER_TAG);
 		len = viewResolverList.getLength();
-		if(len > 0) {
+		if (len > 0) {
 			Element viewResolver = (Element) commonsList.item(0);
-			configuration.setProperty("tulip:screen-uri", tulipItem(viewResolver, "screen-uri", "screen"));
-			configuration.setProperty("tulip:layout-uri", tulipItem(viewResolver, "layout-uri", "layout"));
-			configuration.setProperty("tulip:default-layout", tulipItem(viewResolver, "default-layout", "default"));
-			configuration.setProperty("tulip:screen-key", tulipItem(viewResolver, "screen-key", "screen_placeholder"));
+			screen_uri = parseItem(viewResolver, TEMPLATES_SCREEN_URI_TAG, DEFAULT_TEMPLATES_SCREEN_URI);
+			layout_uri = parseItem(viewResolver, TEMPLATES_LAYOUT_URI_TAG, DEFAULT_TEMPLATES_LAYOUT_URI);
+			default_layout = parseItem(viewResolver, TEMPLATES_DEFAULT_LAYOUT_TAG, DEFAULT_TEMPLATES_DEFAULT_LAYOUT);
+			screen_key = parseItem(viewResolver, TEMPLATES_SCREEN_KEY_TAG, DEFAULT_TEMPLATES_SCREEN_KEY);
 		}
 		NodeList eventCartridgeList = element.getElementsByTagName(EVENT_CARTRIDGE_TAG);
 		len = eventCartridgeList.getLength();
-		if(len > 0) {
-			for(int i = 0; i< len; i++) {
-				configuration.addProperty("tulip:handlers", tulipItem((Element) eventCartridgeList.item(i), "handler", ""));
+		if (len > 0) {
+			for (int i = 0; i < len; i++) {
+				String handler = parseItem((Element) eventCartridgeList.item(i), EVENT_HANDLER_TAG, "");
+				if(StringUtil.isBlank(handler)) {
+					continue;
+				}
+				try {
+					handlers.add(ClassUtils.getDefaultClassLoader().loadClass(handler));
+				} catch (Exception e) {
+					log.error("Load Handler Class Named : " + handler + " Error.", e);
+				}
 			}
 		}
 		NodeList viewControllerList = element.getElementsByTagName(VIEW_CONTROLLER_TAG);
 		len = viewControllerList.getLength();
-		if(len > 0) {
-			for(int i = 0; i< len; i++) {
+		if (len > 0) {
+			for (int i = 0; i < len; i++) {
 				Element controller = (Element) viewControllerList.item(i);
-				String name = controller.getAttribute("name");
-				String controller_uri = controller.getAttribute("controller-uri");
-				Entry entry = new Entry(name, controller_uri);
-				configuration.addProperty("tulip:controllers", entry);
+				String name = controller.getAttribute(CONTROLLER_NAME_ATTR);
+				String controller_uri = controller.getAttribute(CONTROLLER_URI_ATTR);
+				controllers.put(name, controller_uri);
 			}
 		}
 		NodeList velocityEngineList = element.getElementsByTagName(VELOCITY_ENGINE_TAG);
 		len = velocityEngineList.getLength();
-		if(len > 0) {
+		if (len > 0) {
 			Element velocityEngine = (Element) velocityEngineList.item(0);
-			configuration.setProperty("tulip:config-location", velocityEngine.getAttribute("config-location"));
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
-//			configuration.setProperty(key, value);
+			config_location = velocityEngine.getAttribute("config-location");
+			String velocity_configurer = parseItem(element, "velocity-configurer", "", "class");
+			if(StringUtil.isNotBlank(velocity_configurer)) {
+				try {
+					velocity_engine_configurer = ClassUtils.getDefaultClassLoader().loadClass(velocity_configurer);
+				} catch (Exception e) {
+					log.error("Load VelocityEngine Configurer Class Named :" + velocity_configurer + " Error.", e);
+				}
+			}
 			
 		}
 	}
-	
-	private String tulipItem(Element element, String itemName, String defaultValue) {
-		return tulipItem(element, itemName, defaultValue, "value");
+
+	private String parseItem(Element element, String itemName, String defaultValue) {
+		return parseItem(element, itemName, defaultValue, "value");
 	}
-	
-	private String tulipItem(Element element, String itemName, String defaultValue, String key) {
+
+	private String parseItem(Element element, String itemName, String defaultValue, String key) {
 		NodeList itemsList = element.getElementsByTagName(itemName);
-		if(itemsList.getLength() <= 0) {
+		if (itemsList.getLength() <= 0) {
 			return defaultValue;
 		}
 		Element item = (Element) itemsList.item(0);
 		return StringUtil.defaultIfBlank(item.getAttribute(key), defaultValue);
-	}
-	
-	class Entry {
-		
-		final String name;
-		final String controller_uri;
-		
-		public Entry(String name, String controller_uri) {
-			super();
-			this.name = name;
-			this.controller_uri = controller_uri;
-		}
 	}
 }
